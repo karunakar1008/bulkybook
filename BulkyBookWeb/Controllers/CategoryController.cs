225ad576dfc411a4daf2c35a6eff2ae73900030e
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-01 16:47:19 +0530

    Get categories from db

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        public readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext applicationDbContext)
+        {
+            _db = applicationDbContext;
+        }
         public IActionResult Index()
         {
+            var categories = _db.Categories.ToList();
             return View();
         }
     }
