0886568803dbdf3e5d3c9a0f398f3e487d2622d6
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-01 16:24:27 +0530

    Configure ApplicationDbContext class

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -4,5 +4,9 @@ namespace BulkyBookWeb.Data
 {
     public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
     }
 }
