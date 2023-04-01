ca0f16a06473ef39655e00150276761f66d97020
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-01 16:52:58 +0530

    Display categories

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
@@ -12,8 +13,8 @@ namespace BulkyBookWeb.Controllers
         }
         public IActionResult Index()
         {
-            var categories = _db.Categories.ToList();
-            return View();
+            IEnumerable<Category> categories = _db.Categories;
+            return View(categories);
         }
     }
 }
