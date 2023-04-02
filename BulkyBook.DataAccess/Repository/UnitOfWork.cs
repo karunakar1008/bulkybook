5480e77f0e41d601e01058d91f75f4799424ae9c
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-02 14:45:29 +0530

    Include Covertype repository in Unit of Work pattern

--- BulkyBook.DataAccess/Repository/UnitOfWork.cs
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -5,11 +5,13 @@ namespace BulkyBook.DataAccess.Repository
     public class UnitOfWork : IUnitOfWork
     {
         public ICategoryRepository CategoryRepository { get; private set; }
+        public ICoverTypeRepository CoverTypeRepository { get; private set; }
         public readonly ApplicationDbContext _db;
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             CategoryRepository = new CategoryRepository(_db);
+            CoverTypeRepository= new CoverTypeRepository(_db);
         }
         public void Save()
         {
