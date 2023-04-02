25c9c3ffab673048ffec856edccd2db6553784f3
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-02 13:58:26 +0530

    Implement UnitOfWork pattern,wrapper around Repository pattern

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ICategoryRepository CategoryRepository { get; private set; }
+        public readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            CategoryRepository = new CategoryRepository(_db);
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
