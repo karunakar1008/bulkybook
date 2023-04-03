7bb66ac91432ab0f8f755cfad7adfbc51ad8d7e9
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-03 23:59:42 +0530

    Include navigation properties

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -11,7 +11,19 @@ function loadDataTable() {
             { "data": "title", "width": "15%" },
             { "data": "isbn", "width": "15%" },
             { "data": "price", "width": "15%" },
-            { "data": "author", "width": "15%" }
+            { "data": "author", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="w-75 btn-group" role="group">
+                            <a href='/admin/product/upsert?id=${data}' class='btn btn-primary mx-2'><i class="bi bi-pencil-square" ></i> &nbsp; Edit</a>
+                            <a onClick=Delete('/Admin/Product/Delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash3"></i> &nbsp; Delete</a>
+                            </div>
+                        `
+                }, "width": "15%"
+            },
         ]
     })
 }
