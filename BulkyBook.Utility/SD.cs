6fd4fa2359959dcb146ab11548dd04f9fd24a7a1
karunakar bhogyari <karunakar1008@gmail.com>
2023-04-05 10:47:40 +0530

    Seed some default roles on user landing register page

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -2,6 +2,13 @@
 {
     public static class SD
     {
+        //Indivisual user
+        public const string Role_User_Indi = "Individual";
 
+        //COmpany user
+        public const string Role_User_Comp = "Company";
+        //Mangement roles
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
     }
 }
